c9fcd214ebdfa0abe87cc12bdd04d43815362058
Soukaïna <soukaina@soukainas-macbook-pro.home>
2023-08-28 21:10:13 +0200

    complete train model

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -3,7 +3,7 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
 from ml.data import process_data
-from ml.model import train_model, compute_model_metrics
+from ml.model import train_model, compute_model_metrics, inference
 from sklearn.metrics import accuracy_score
 
 def run_training():
@@ -39,8 +39,8 @@ def run_training():
 
     model = train_model(X_train, y_train)
 
-    preds = model.predict(X_train)
-
+    preds = inference(model, X_train)
+    
     precision, recall, fbeta = compute_model_metrics(y_train, preds)
 
     print("precision : {}".format(precision))
