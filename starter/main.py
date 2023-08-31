9499e39fa5b84b41d1c5001ca1a0216e67b32ca5
Soukaïna <soukaina@soukainas-macbook-pro.home>
2023-08-31 21:58:05 +0200

    fix linting

--- starter/main.py
+++ starter/main.py
@@ -1,12 +1,15 @@
-from typing import Union, List, Any
+from typing import Any
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 from fastapi.responses import HTMLResponse
 import pandas as pd
 from fastapi.encoders import jsonable_encoder
 from starter.train_model import run_training, run_predict
+
+
 class PredictionResults(BaseModel):
     expected: dict
+
 
 class CensusDataInputSchema(BaseModel):
     age: int = Field(examples=[27])
@@ -19,15 +22,18 @@ class CensusDataInputSchema(BaseModel):
     relationship: str = Field(examples=["Unmarried"])
     race: str = Field(examples=["White"])
     sex: str = Field(examples=["Female"])
-    hours_per_week : int = Field(alias="hours-per-week", examples=[44])
-    capital_gain: int = Field(alias ="capital-gain", examples=[0])
+    hours_per_week: int = Field(alias="hours-per-week", examples=[44])
+    capital_gain: int = Field(alias="capital-gain", examples=[0])
     capital_loss: int = Field(alias="capital-loss", examples=[0])
-    native_country: str = Field(alias = "native-country", examples=["United-Stated"])
+    native_country: str = Field(
+        alias="native-country",
+        examples=["United-Stated"])
 
-    
+
 app = FastAPI(
     title="Project Deployment API",
     version="0.0.1")
+
 
 @app.get("/")
 async def index():
@@ -45,20 +51,18 @@ async def index():
 
     return HTMLResponse(content=body)
 
-@app.post("/model/", status_code = 200)
-async def predict(input_data: CensusDataInputSchema) -> Any :
 
-    data = pd.read_csv('data/census.csv')
+@app.post("/model/", status_code=200)
+async def predict(input_data: CensusDataInputSchema) -> Any:
 
-    model, encoder, lb, cat_features, mapping = run_training(data)
-    
+    model, encoder, lb, cat_features, mapping = run_training()
+
     input_df = pd.DataFrame(jsonable_encoder(input_data), index=[0])
-    
+
     preds = run_predict(input_df, model, cat_features, encoder, lb, mapping)
 
     results = {
-        "expected_salary" : preds
+        "expected_salary": preds
     }
-    
-    return results
 
+    return results
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,14 +1,13 @@
 # Script to train machine learning model.
-
-from sklearn.model_selection import train_test_split
+import logging
+import joblib
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from ml.data import process_data, get_salary_class
 from ml.model import train_model, compute_model_metrics, inference
-from sklearn.metrics import accuracy_score
-import joblib
-import logging
 
-    
+
 def run_training():
 
     # Add the necessary imports for the starter code.
@@ -19,8 +18,9 @@ def run_training():
     # Remove spaces from columns names
     data.columns = [colname.replace(' ', '') for colname in data.columns]
 
-    print("data.shape : {}".format(data.shape))
-    # Optional enhancement, use K-fold cross validation instead of a train-test split.
+    print(f"data.shape : {format(data.shape)}")
+    # Optional enhancement, use K-fold cross validation instead of a
+    # train-test split.
     train, test = train_test_split(data, test_size=0.20, random_state=0)
 
     cat_features = [
@@ -41,8 +41,8 @@ def run_training():
     # Proces the test data with the process_data function.
 
     X_test, y_test, encoder, lb = process_data(
-        test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
-    )
+        test, categorical_features=cat_features,
+        label="salary", training=False, encoder=encoder, lb=lb)
     print("test.shape : {}".format(X_test.shape))
 
     # Train and save a model.
@@ -67,16 +67,16 @@ def run_training():
 
 
 def run_predict(input_data, model, cat_features, encoder, lb, mapping):
-        
+
     input_data, y, encoder, lb = process_data(
-        input_data, categorical_features=cat_features, 
+        input_data, categorical_features=cat_features,
         training=False, encoder=encoder, lb=lb)
-    
+
     preds = inference(model, input_data)
     preds = [key for key, value in mapping.items() if value == preds][0]
     return preds
-    
+
 
 if __name__ == "__main__":
-    
+
     model, encoder, lb, cat_features, mapping = run_training()
