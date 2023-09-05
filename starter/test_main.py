7c3f5fbf3f49023b9f8470c4c544acc25501d2ba
Soukaïna <soukaina@soukainas-macbook-pro.home>
2023-09-05 09:43:08 +0200

    add test get root

--- starter/test_main.py
+++ starter/test_main.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+from fastapi.responses import HTMLResponse
 
 from main import app
 import pandas as pd
@@ -8,7 +9,8 @@ client = TestClient(app)
 def test_api_get_root():
     response = client.get("/")
     assert response.status_code == 200
-    
+    assert response.text ==  "<html><body style='padding: 10px;'><h1>Welcome to the API</h1><div>Check the docs: <a href='/docs'>here</a></div></body></html>"
+
 
 # def test_post_data_fail():
 #     data = {"feature_1": -5, "feature_2": "test string"}
@@ -18,7 +20,7 @@ def test_api_get_root():
 
 def test_make_prediction() -> None:
     # Given
-    test_data = pd.read_csv('starter/data/census.csv')
+    test_data = pd.read_csv('data/census.csv')
     test_data = test_data.iloc[0,:]
 
     payload = test_data.to_dict()
